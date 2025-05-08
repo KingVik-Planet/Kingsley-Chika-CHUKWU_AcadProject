9eb3f794c5db3548abc1814c3494da432f602488
Kingsley Chika CHUKWU <107149587+KingVik-Planet@users.noreply.github.com>
2025-05-09 01:03:50 +0200

    Update Open_Building.js

--- Files Folder/UNILAK/Open_Building.js
+++ Files Folder/UNILAK/Open_Building.js
@@ -1,1 +1,34 @@
+// Visualization of GOOGLE/Research/open-buildings/v3/polygons.
+
+// Load the building polygons
+var t = ee.FeatureCollection('GOOGLE/Research/open-buildings/v3/polygons');
+
+// Define your region of interest (roi) as a geometry
+var roi = roi;
+
+// Add roi to the map
+Map.addLayer(roi, {}, 'Rulindo Study Area Building');
+Map.centerObject(roi, 12);
+
+// Filter buildings based on confidence levels
+var t_065_070 = t.filter('confidence >= 0.65 && confidence < 0.7');
+var t_070_075 = t.filter('confidence >= 0.7 && confidence < 0.75');
+var t_gte_075 = t.filter('confidence >= 0.75');
+
+// Add layers to the map
+Map.addLayer(t_065_070, {color: 'FF0000'}, 'Buildings confidence [0.65; 0.7)');
+Map.addLayer(t_070_075, {color: 'FFFF00'}, 'Buildings confidence [0.7; 0.75)');
+Map.addLayer(t_gte_075, {color: '00FF00'}, 'Buildings confidence >= 0.75');
+
+Map.setOptions('SATELLITE');
+
+// Filter the FeatureCollection based on roi and then export
+var t_roi = t.filterBounds(roi);
+Export.table.toDrive({
+  collection: t_roi,
+  description: 'OpenBuildings',
+  fileFormat: 'GeoJSON', // Change to 'SHP' for Shapefile format
+  folder: 'Google_Earth_Engine_Export', // Change to the desired folder in your Google Drive
+  fileNamePrefix: 'Buildings'
+});
 
